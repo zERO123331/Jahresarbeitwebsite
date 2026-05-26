4362ee3fabc04ca97768573371e5958a2e45bea3
caos <netzpeer@gmail.com>
2026-05-26 13:36:22 +0200

    working on Validation and that stuff, now with the files

---
+++ internal/validator/validator.go
@@ -0,0 +1,38 @@
+package validator
+
+import (
+	"regexp"
+	"slices"
+)
+
+var (
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
+type Validator struct {
+	Errors map[string][]string
+}
+
+func New() *Validator {
+	return &Validator{Errors: make(map[string][]string)}
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+func (v *Validator) AddError(key, message string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = []string{message}
+	}
+}
+
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	return slices.Contains(permittedValues, value)
+}
